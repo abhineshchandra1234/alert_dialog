e19c0fe5e62f691c9768c37140ce7a0044798a70
abhinesh <abhinesh.c@eegrab.com>
2021-02-15 12:06:47 +0530

    first commit

---
+++ app/src/main/java/com/eegrab/alertdialog/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.eegrab.alertdialog
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    override fun onBackPressed() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Are you sure!")
+        builder.setMessage("Do you want to close the app?")
+        builder.setPositiveButton("Yes",{ dialogInterface: DialogInterface, i: Int ->
+            finish()
+        })
+        builder.setNegativeButton("No",{ dialogInterface: DialogInterface, i: Int -> })
+        builder.show()
+    }
+}
